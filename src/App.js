dfef94de5a87dfea1377de5cacdfc8239b54e503
akhmdarya <53120495+akhmdarya@users.noreply.github.com>
2020-11-02 23:41:53 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Greetings from "./Greetings";
+import TodoList from './Todo/TodoList'
+
+
+
+function App() {
+  return (
+    <div className="wrapper">
+      <TodoList/>
+
+    </div>
+  );
+}
+
+
+export default App;
